1bc0cf804be772f9d245a6fe339307196306d3bb
Yurii Melnyk <yurleomel@gmail.com>
2026-03-26 15:29:12 +0200

    aded function that gets linked list node by index

---
+++ Linked_Lists_Get_Nth_Node/code.py
@@ -0,0 +1,20 @@
+from preloaded import Node
+
+# class Node(object):
+#     """Node class for reference"""
+#     def __init__(self, data, next=None):
+#         self.data = data
+#         self.next = next
+    
+def get_nth(node, index):
+    i = 0
+
+    while node:
+        if index == i:
+            return node
+        
+        node = node.next
+        i += 1
+
+    raise Exception("Index out of range")
+  
